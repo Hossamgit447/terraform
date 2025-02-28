620c9a993c5639022e804f7558568ecd1a9ce8ee
Hossamgit447 <hossamgithub@gmail.com>
2025-02-27 21:03:10 -0800

    lock state add

--- newmain.tf
+++ newmain.tf
@@ -1,3 +1,15 @@
-module "github-repo" {
-  source = "github.com/enggalal/dolfined_repo.git"
+# module "github-repo" {
+#   source = "github.com/enggalal/dolfined_repo.git"
+# }
+resource "aws_s3_bucket" "s3_backend" {
+  bucket = "hossambucket1234132"
+  tags = {
+    Name="s3_backend"
+  }
+ 
+}
+resource "aws_iam_user" "lb" {
+  name  = "dev_user${count.index}"
+  count = 3
+  path  = "/system/"
 }
--- setting.tf
+++ setting.tf
@@ -1,9 +1,18 @@
 provider "aws" {
   profile = "Terraform-user"
-  region  = "us-east-1"
+  region = "us-east-1"
 }
 provider "aws" {
-  alias   = "non-default"
+  alias = "non-default"
   profile = "Terraform-user2"
-  region  = "us-west-1"
+  region = "us-west-1"
+}
+terraform {
+  backend "s3" {
+    bucket="hossambucket1234132"
+    key = "network/backend"
+    region = "us-east-1"
+    profile = "Terraform-user" 
+    dynamodb_table = "state_lock_table"
+  }
 }
