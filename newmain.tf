2ffc0c569e7abe1998308241c9808fda51587206
Hossamgit447 <hossamgithub@gmail.com>
2025-02-27 19:40:35 -0800

    changes made

--- newmain.tf
+++ newmain.tf
@@ -1,14 +1,3 @@
-resource "aws_instance" "ec2" {
-  instance_type = lookup(var.type,terraform.workspace)
-  ami = "ami-05b10e08d247fb927"
-  
-}
-variable "type" {
-  type = map(any)
-  default = {
-    prod="t2.large"
-    default="t2.micro"
-    dev="t3.meduim"
-  }
-
+module "github-repo" {
+  source = "github.com/enggalal/dolfined_repo.git"
 }
--- variable.tf
+++
@@ -1,3 +0,0 @@
-variable "vpn_ip" {
-  default = "1.1.1.1/32"
-}
